99a556dd5c9cd056af0bf6e2a1ccd96b55d4f5a1
huzaifa003 <buntyhuzaifa3@gmail.com>
2023-10-26 04:41:53 +0500

    Improved DIshes

--- Dishes.js
+++ Dishes.js
@@ -1,67 +1,81 @@
 import React from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  SectionList,
-  StatusBar,
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    SectionList,
+    StatusBar,
 } from 'react-native';
 
 const DATA = [
-  {
-    title: 'Main dishes',
-    data: ['Pizza', 'Burger', 'Risotto'],
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-  },
-  {
-    title: 'Drinks',
-    data: ['Water', 'Coke', 'Beer'],
-  },
-  {
-    title: 'Desserts',
-    data: ['Cheese Cake', 'Ice Cream'],
-  },
+    {
+        title: 'Main dishes',
+        data: ['Pizza', 'Burger', 'Risotto'],
+    },
+    {
+        title: 'Sides',
+        data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+    },
+    {
+        title: 'Drinks',
+        data: ['Water', 'Coke', 'Beer'],
+    },
+    {
+        title: 'Desserts',
+        data: ['Cheese Cake', 'Ice Cream'],
+    },
 ];
 
 const Dishes = () => (
-  <SafeAreaView style={styles.container}>
-    <SectionList
-      sections={DATA}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({item}) => (
-        <View style={styles.item}>
-          <Text style={styles.title}>{item}</Text>
-        </View>
-      )}
-      renderSectionHeader={({section: {title}}) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />
-  </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+        <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => item + index}
+
+            renderItem={({ section, item }) => (
+                <>
+
+                    {console.log("HELO")}
+                    {console.log(section.data.length)}
+                    {section.data.length > 2 ? 
+
+                        <Text style={styles.title}>{item} </Text>
+
+                     : ""}
+
+                </>
+            )}
+            renderSectionHeader={({ section: { title, data } }) => (
+
+                <View>
+
+                    {data.length > 2 ? <Text style={styles.header}>{title}</Text> : ""}
+                </View>
+
+            )}
+        />
+    </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-  },
-  header: {
-    fontSize: 32,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-  },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+        marginHorizontal: 16,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+    },
+    header: {
+        fontSize: 32,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+    },
 });
 
 export default Dishes;
