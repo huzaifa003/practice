a1ad7d2c2e804d49faa897de3bc280f7a103672c
huzaifa003 <buntyhuzaifa3@gmail.com>
2023-10-26 02:09:57 +0500

    Added product

--- App.js
+++ App.js
@@ -5,11 +5,35 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Home';
 import Dashboard from './Dashboard';
+import { FlatList } from 'react-native-gesture-handler';
+import Product from './Product';
+import Dishes from './Dishes';
+
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
+
+      <Stack.Screen
+          name="Dishes"
+          component={Dishes}
+          options={{
+            headerShown: false
+          }}
+        />
+
+
+
+      <Stack.Screen
+          name="Product"
+          component={Product}
+          options={{
+            headerShown: false
+          }}
+        />
+
 
         <Stack.Screen
           name="Home"
